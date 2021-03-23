fc78c72f1221f5d3b2bb6109212f39ea59aba793
kitonetto <baixokito@gmail.com>
2021-03-23 20:56:04 -0300

    First class - Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
 
+		// Instanciando e testando a classe Position
+		Position pos = new Position(3, 5);
+
+		System.out.println(pos);
 	}
 
 }
